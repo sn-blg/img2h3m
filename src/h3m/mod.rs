d4a9e96279d8b9df10136e6cf0e7e9516b4cc157
sn-blg <snb1g@yandex.ru>
2021-10-27 01:49:36 +0300

    add libflate usage

--- src/h3m/mod.rs
+++ src/h3m/mod.rs
@@ -1,15 +1,25 @@
+use libflate::gzip::{Decoder, Encoder};
 use result::H3mResult;
+use std::io::{self, Read, Write};
 
 pub mod result;
 
-pub struct H3m {}
+pub struct H3m {
+    data: Vec<u8>,
+}
 
 impl H3m {
-    pub fn load() -> H3mResult<H3m> {
-        Ok(H3m {})
+    pub fn load<R: io::Read>(input: R) -> H3mResult<H3m> {
+        let mut decoder = Decoder::new(input)?;
+        let mut data = Vec::new();
+        decoder.read_to_end(&mut data)?;
+        Ok(H3m { data })
     }
 
-    pub fn save(&self) -> H3mResult<()> {
+    pub fn save<W: io::Write>(&self, output: W) -> H3mResult<()> {
+        let mut encoder = Encoder::new(output)?;
+        encoder.write_all(&self.data)?;
+        encoder.finish().into_result()?;
         Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
-use std::error::Error;
-use h3m::H3m;
 pub use config::Config;
+use h3m::H3m;
+use std::error::Error;
+use std::fs::File;
 
+pub mod cli;
 mod config;
 mod h3m;
-pub mod cli;
 
-pub fn run(_config: Config) -> Result<(), Box<dyn Error>> {
-    let h3m = H3m::load()?;
-    h3m.save()?;
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let _image_file = File::open(config.image_path)?;
+
+    let input_map_file = File::open(&config.map_path)?;
+    let h3m = H3m::load(input_map_file)?;
+
+    let output_map_file = File::create(&config.map_path)?;
+    h3m.save(output_map_file)?;
     Ok(())
 }
