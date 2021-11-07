bfdb545fb9fb8677a2bdd43121aec8352e92a18a
sn-blg <snb1g@yandex.ru>
2021-11-07 17:32:12 +0300

    done fix map draft impl

--- src/lib.rs
+++ src/lib.rs
@@ -17,15 +17,6 @@ struct MapPixel {
     surface: Surface,
 }
 
-impl MapPixel {
-    fn new(original_color: Rgb<u8>, surface: Surface) -> MapPixel {
-        MapPixel {
-            surface,
-            original_color,
-        }
-    }
-}
-
 struct Map {
     size: usize,
     pixels: Vec<Option<MapPixel>>,
@@ -41,23 +32,204 @@ impl Map {
         }
     }
 
+    fn calc_index(&self, row: usize, column: usize) -> usize {
+        row * self.size + column
+    }
+
     fn set_pixel(&mut self, row: usize, column: usize, pixel: Rgb<u8>) {
         let ground_only = false;
         let surface = self.palettes.nearest_surface(&pixel, ground_only);
-        let index = row * self.size + column;
+        let index = self.calc_index(row, column);
 
-        self.pixels[index] = Some(MapPixel::new(pixel, surface));
+        self.pixels[index] = Some(MapPixel {
+            surface,
+            original_color: pixel,
+        });
+    }
+
+    fn try_get_pixel(&self, row: i64, column: i64) -> Option<MapPixel> {
+        if (row < 0) || (column < 0) {
+            return None;
+        }
+
+        let row = row as usize;
+        let column = column as usize;
+
+        if (row >= self.size) || (column >= self.size) {
+            return None;
+        }
+
+        let index = self.calc_index(row, column);
+        self.pixels[index]
+    }
+
+    fn is_problem_pixel(&self, row: usize, column: usize, pixel: &MapPixel) -> bool {
+        fn test_pixel_neighbors(
+            base: &MapPixel,
+            same: bool,
+            neighbors: &[Option<MapPixel>],
+        ) -> bool {
+            for neighbour in neighbors {
+                if let Some(neighbour) = neighbour {
+                    let is_same_neighbour = neighbour.surface == base.surface;
+                    if (same && !is_same_neighbour) || (!same && is_same_neighbour) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            true
+        }
+
+        fn is_all_some_and_not_same(base: &MapPixel, neighbors: &[Option<MapPixel>]) -> bool {
+            test_pixel_neighbors(base, false, neighbors)
+        }
+
+        fn is_all_some_and_same(base: &MapPixel, neighbors: &[Option<MapPixel>]) -> bool {
+            test_pixel_neighbors(base, true, neighbors)
+        }
+
+        if pixel.surface.is_ground() {
+            return false;
+        }
+
+        let row = row as i64;
+        let column = column as i64;
+
+        if is_all_some_and_not_same(
+            pixel,
+            &[
+                self.try_get_pixel(row - 1, column),
+                self.try_get_pixel(row + 1, column),
+            ],
+        ) {
+            return true;
+        }
+
+        if is_all_some_and_not_same(
+            pixel,
+            &[
+                self.try_get_pixel(row, column - 1),
+                self.try_get_pixel(row, column + 1),
+            ],
+        ) {
+            return true;
+        }
+
+        ////////////
+        if is_all_some_and_not_same(
+            pixel,
+            &[
+                self.try_get_pixel(row - 1, column - 1),
+                self.try_get_pixel(row - 1, column),
+                self.try_get_pixel(row, column - 1),
+                self.try_get_pixel(row + 1, column + 1),
+            ],
+        ) {
+            return true;
+        }
+
+        if is_all_some_and_not_same(
+            pixel,
+            &[
+                self.try_get_pixel(row - 1, column - 1),
+                self.try_get_pixel(row + 1, column),
+                self.try_get_pixel(row, column + 1),
+                self.try_get_pixel(row + 1, column + 1),
+            ],
+        ) {
+            return true;
+        }
+
+        if is_all_some_and_not_same(
+            pixel,
+            &[
+                self.try_get_pixel(row + 1, column - 1),
+                self.try_get_pixel(row, column - 1),
+                self.try_get_pixel(row + 1, column),
+                self.try_get_pixel(row - 1, column + 1),
+            ],
+        ) {
+            return true;
+        }
+
+        if is_all_some_and_not_same(
+            pixel,
+            &[
+                self.try_get_pixel(row + 1, column - 1),
+                self.try_get_pixel(row - 1, column),
+                self.try_get_pixel(row, column + 1),
+                self.try_get_pixel(row - 1, column + 1),
+            ],
+        ) {
+            return true;
+        }
+        ///////////////
+
+        if is_all_some_and_same(
+            pixel,
+            &[
+                self.try_get_pixel(row, column - 1),
+                self.try_get_pixel(row, column + 1),
+                self.try_get_pixel(row - 1, column),
+                self.try_get_pixel(row + 1, column),
+            ],
+        ) {
+            if is_all_some_and_not_same(
+                pixel,
+                &[
+                    self.try_get_pixel(row - 1, column - 1),
+                    self.try_get_pixel(row + 1, column + 1),
+                ],
+            ) {
+                return true;
+            }
+
+            if is_all_some_and_not_same(
+                pixel,
+                &[
+                    self.try_get_pixel(row - 1, column + 1),
+                    self.try_get_pixel(row + 1, column - 1),
+                ],
+            ) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn fix_problem_pixel(&mut self, index: usize) {
+        let ground_only = true;
+        let pixel = &mut self.pixels[index];
+        if let Some(pixel) = pixel {
+            pixel.surface = self
+                .palettes
+                .nearest_surface(&pixel.original_color, ground_only);
+        }
+    }
+
+    fn fix_impl(&mut self) -> bool {
+        let mut has_problem = false;
+        for row in 0..self.size {
+            for column in 0..self.size {
+                let index = self.calc_index(row, column);
+                let pixel = self.pixels[index];
+
+                if let Some(pixel) = pixel {
+                    if self.is_problem_pixel(row, column, &pixel) {
+                        has_problem = true;
+                        self.fix_problem_pixel(index);
+                    }
+                }
+            }
+        }
+        has_problem
     }
 
     fn fix(&mut self) {
-        let ground_only = true;
-        for pixel in self.pixels.iter_mut().flatten() {
-            if !pixel.surface.is_ground() {
-                pixel.surface = self
-                    .palettes
-                    .nearest_surface(&pixel.original_color, ground_only);
-            }
-        }
+        while self.fix_impl() {}
     }
 
     fn into_surfaces(self) -> Vec<Option<Surface>> {
