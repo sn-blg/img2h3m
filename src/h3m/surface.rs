bed1b877d0ec61b60eacc0aaa9a5b871cd3e2e07
sn-blg <snb1g@yandex.ru>
2021-11-12 23:14:35 +0300

    add surface_obstacle_rgb_color func

--- src/h3m/surface.rs
+++ src/h3m/surface.rs
@@ -32,23 +32,6 @@ impl Surface {
         }
     }
 
-    pub fn rgb_color(&self) -> [u8; 3] {
-        match *self {
-            Surface::Dirt => [0x52, 0x39, 0x08],
-            Surface::Sand => [0xDE, 0xCE, 0x8C],
-            Surface::Grass => [0x00, 0x42, 0x00],
-            Surface::Snow => [0xB5, 0xC6, 0xC6],
-            Surface::Swamp => [0x4A, 0x84, 0x6B],
-            Surface::Rough => [0x84, 0x73, 0x31],
-            Surface::Subterranean => [0x84, 0x31, 0x00],
-            Surface::Lava => [0x4A, 0x4A, 0x4A],
-            Surface::Highland => [0x29, 0x73, 0x18],
-            Surface::Wasteland => [0xBD, 0x5A, 0x08],
-            Surface::Water => [0x08, 0x52, 0x94],
-            Surface::Rock => [0x00, 0x00, 0x00],
-        }
-    }
-
     pub fn is_ground(&self) -> bool {
         !matches!(*self, Surface::Water | Surface::Rock)
     }
--- src/map_image/palettes.rs
+++ src/map_image/palettes.rs
@@ -3,13 +3,49 @@ use crate::h3m::Surface;
 use delta_e::DE2000;
 use image::Rgb;
 
+type RgbColor = [u8; 3];
+
+fn surface_rgb_color(surface: Surface) -> RgbColor {
+    match surface {
+        Surface::Dirt => [0x52, 0x39, 0x08],
+        Surface::Sand => [0xDE, 0xCE, 0x8C],
+        Surface::Grass => [0x00, 0x42, 0x00],
+        Surface::Snow => [0xB5, 0xC6, 0xC6],
+        Surface::Swamp => [0x4A, 0x84, 0x6B],
+        Surface::Rough => [0x84, 0x73, 0x31],
+        Surface::Subterranean => [0x84, 0x31, 0x00],
+        Surface::Lava => [0x4A, 0x4A, 0x4A],
+        Surface::Highland => [0x29, 0x73, 0x18],
+        Surface::Wasteland => [0xBD, 0x5A, 0x08],
+        Surface::Water => [0x08, 0x52, 0x94],
+        Surface::Rock => [0x00, 0x00, 0x00],
+    }
+}
+
+fn surface_obstacle_rgb_color(surface: Surface) -> Option<RgbColor> {
+    match surface {
+        Surface::Dirt => Some([0x39, 0x29, 0x08]),
+        Surface::Sand => Some([0xA5, 0x9C, 0x6B]),
+        Surface::Grass => Some([0x00, 0x31, 0x00]),
+        Surface::Snow => Some([0x8C, 0x9C, 0x9C]),
+        Surface::Swamp => Some([0x21, 0x5A, 0x42]),
+        Surface::Rough => Some([0x63, 0x52, 0x21]),
+        Surface::Subterranean => Some([0x5A, 0x08, 0x00]),
+        Surface::Lava => Some([0x29, 0x29, 0x29]),
+        Surface::Highland => Some([0x21, 0x52, 0x10]),
+        Surface::Wasteland => Some([0x9C, 0x42, 0x08]),
+        Surface::Water => Some([0x00, 0x29, 0x6B]),
+        Surface::Rock => None,
+    }
+}
+
 struct Color {
     surface_info: SurfaceInfo,
-    rgb_color: [u8; 3],
+    rgb_color: RgbColor,
 }
 
 impl Color {
-    fn new(surface: Surface, obstacle: bool, rgb_color: [u8; 3]) -> Color {
+    fn new(surface: Surface, obstacle: bool, rgb_color: RgbColor) -> Color {
         Color {
             surface_info: SurfaceInfo { surface, obstacle },
             rgb_color,
@@ -25,21 +61,35 @@ pub struct Palettes {
 }
 
 impl Palettes {
-    pub fn new(_obstacles: bool) -> Palettes {
+    pub fn new(obstacles: bool) -> Palettes {
         let mut palettes = Palettes {
             ground: Vec::new(),
             all: Vec::new(),
         };
 
         let mut add_surface = |surface: Surface| {
-            palettes
-                .all
-                .push(Color::new(surface, false, surface.rgb_color()));
+            let color = surface_rgb_color(surface);
+
+            palettes.all.push(Color::new(surface, false, color));
 
             if surface.is_ground() {
-                palettes
-                    .ground
-                    .push(Color::new(surface, false, surface.rgb_color()));
+                palettes.ground.push(Color::new(surface, false, color));
+            }
+
+            let obstacle_color = if obstacles {
+                surface_obstacle_rgb_color(surface)
+            } else {
+                None
+            };
+
+            if let Some(obstacle_color) = obstacle_color {
+                palettes.all.push(Color::new(surface, true, obstacle_color));
+
+                if surface.is_ground() {
+                    palettes
+                        .ground
+                        .push(Color::new(surface, true, obstacle_color));
+                }
             }
         };
 
