908e840439bef0b2eff51c8edeba3e2b2d1d4c83
sn-blg <snb1g@yandex.ru>
2021-12-06 02:19:06 +0300

    use TileIndexSet in TileCodeGenerator

--- src/h3m/terrain_map/tile_code_generator.rs
+++ src/h3m/terrain_map/tile_code_generator.rs
@@ -1,34 +1,69 @@
+use super::tile_index_set::TileIndexSet;
 use super::tile_type::TileType;
 use crate::h3m::Terrain;
-use rand::Rng;
+use std::collections::HashMap;
 
-pub struct TileCodeGenerator {}
+pub struct TileCodeGenerator {
+    tile_indexes: HashMap<Terrain, HashMap<TileType, TileIndexSet>>,
+}
 
 impl TileCodeGenerator {
     pub fn new() -> TileCodeGenerator {
-        TileCodeGenerator {}
+        let tile_indexes = HashMap::from([
+            (
+                Terrain::Dirt,
+                HashMap::from([(TileType::Common, TileIndexSet::new(21..=44, 100))]),
+            ),
+            (
+                Terrain::Sand,
+                HashMap::from([(TileType::Common, TileIndexSet::new(0..=23, 100))]),
+            ),
+            (
+                Terrain::Grass,
+                HashMap::from([(TileType::Common, TileIndexSet::new(49..=72, 100))]),
+            ),
+            (
+                Terrain::Snow,
+                HashMap::from([(TileType::Common, TileIndexSet::new(49..=72, 100))]),
+            ),
+            (
+                Terrain::Swamp,
+                HashMap::from([(TileType::Common, TileIndexSet::new(49..=72, 100))]),
+            ),
+            (
+                Terrain::Rough,
+                HashMap::from([(TileType::Common, TileIndexSet::new(49..=72, 100))]),
+            ),
+            (
+                Terrain::Subterranean,
+                HashMap::from([(TileType::Common, TileIndexSet::new(49..=72, 100))]),
+            ),
+            (
+                Terrain::Lava,
+                HashMap::from([(TileType::Common, TileIndexSet::new(49..=72, 100))]),
+            ),
+            (
+                Terrain::Highland,
+                HashMap::from([(TileType::Common, TileIndexSet::new(77..=117, 100))]),
+            ),
+            (
+                Terrain::Wasteland,
+                HashMap::from([(TileType::Common, TileIndexSet::new(77..=117, 100))]),
+            ),
+            (
+                Terrain::Water,
+                HashMap::from([(TileType::Common, TileIndexSet::new(21..=32, 100))]),
+            ),
+            (
+                Terrain::Rock,
+                HashMap::from([(TileType::Common, TileIndexSet::new(0..=7, 100))]),
+            ),
+        ]);
+
+        TileCodeGenerator { tile_indexes }
     }
 
     pub fn generate(&self, terrain: Terrain, tile_type: TileType) -> u8 {
-        assert_eq!(tile_type, TileType::Common);
-        random_tile_code(terrain)
+        self.tile_indexes[&terrain][&tile_type].random_index()
     }
-}
-
-fn random_tile_code(terrain: Terrain) -> u8 {
-    let range = match terrain {
-        Terrain::Dirt => 21..=44,
-        Terrain::Sand => 0..=23,
-        Terrain::Grass => 49..=72,
-        Terrain::Snow => 49..=72,
-        Terrain::Swamp => 49..=72,
-        Terrain::Rough => 49..=72,
-        Terrain::Subterranean => 49..=72,
-        Terrain::Lava => 49..=72,
-        Terrain::Highland => 77..=117,
-        Terrain::Wasteland => 77..=117,
-        Terrain::Water => 21..=32,
-        Terrain::Rock => 0..=7,
-    };
-    rand::thread_rng().gen_range(range)
 }
--- src/h3m/terrain_map/tile_index_set.rs
+++ src/h3m/terrain_map/tile_index_set.rs
@@ -1,19 +1,24 @@
 use hashbag::HashBag;
 use rand::Rng;
-use std::ops::Range;
+use std::ops::RangeInclusive;
 
 #[derive(Clone)]
 pub struct TileIndexSet(HashBag<u8>);
 
 impl TileIndexSet {
-    pub fn new() -> TileIndexSet {
-        TileIndexSet(HashBag::new())
+    pub fn new(range: RangeInclusive<u8>, frequency: usize) -> TileIndexSet {
+        let mut index_set = HashBag::new();
+        for index in range {
+            index_set.insert_many(index, frequency);
+        }
+        TileIndexSet(index_set)
     }
 
-    pub fn add_tiles(&mut self, range: Range<u8>, frequency: usize) {
+    pub fn with_tiles(mut self, range: RangeInclusive<u8>, frequency: usize) -> TileIndexSet {
         for index in range {
             self.0.insert_many(index, frequency);
         }
+        self
     }
 
     pub fn random_index(&self) -> u8 {
