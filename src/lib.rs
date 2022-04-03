150269785e0ea58737ec2d1d317bff00cd842766
sn-blg <snb1g@yandex.ru>
2022-04-03 15:47:08 +0300

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -12,21 +12,25 @@ mod config;
 mod h3m;
 mod map_image;
 
+struct MapImageParams {
+    one_tile_water: bool,
+    obstacles: bool,
+    transparent_color: Option<RgbColor>,
+}
+
 impl MapImage {
     fn from_image(
         image_path: impl Into<String>,
         map_size: usize,
-        one_tile_water: bool,
-        obstacles: bool,
-        transparent_color: Option<RgbColor>,
+        map_image_params: &MapImageParams,
     ) -> Result<MapImage, Box<dyn Error>> {
-        let mut map_image = MapImage::new(map_size, one_tile_water, obstacles);
+        let mut map_image = MapImage::new(map_size, map_image_params.one_tile_water, map_image_params.obstacles);
         let img = ImageReader::open(image_path.into())?.decode()?.into_rgb8();
         let is_transparent_color = |pixel: &Rgb<u8>| {
-            if transparent_color.is_none() {
+            if map_image_params.transparent_color.is_none() {
                 false
             } else {
-                Some(pixel.0) == transparent_color
+                Some(pixel.0) == map_image_params.transparent_color
             }
         };
 
@@ -47,20 +51,16 @@ impl H3m {
         &mut self,
         image_path: impl Into<String>,
         underground: bool,
-        one_tile_water: bool,
-        obstacles: bool,
-        transparent_color: Option<RgbColor>,
+        map_image_params: &MapImageParams,
     ) -> Result<(), Box<dyn Error>> {
         let mut map_image = MapImage::from_image(
             image_path,
             self.map_size(),
-            one_tile_water,
-            obstacles,
-            transparent_color,
+            map_image_params
         )?;
         map_image.fix();
         let surfaces = map_image.surfaces();
-        self.set_surfaces(one_tile_water, obstacles, underground, &surfaces)?;
+        self.set_surfaces(map_image_params.one_tile_water, map_image_params.obstacles, underground, &surfaces)?;
 
         Ok(())
     }
@@ -70,13 +70,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let input_map_file = File::open(&config.map_path)?;
     let mut h3m = H3m::load(input_map_file)?;
 
+    let map_image_params = MapImageParams {
+        one_tile_water: config.one_tile_water,
+        obstacles: config.obstacles,
+        transparent_color: config.transparent_color,
+    };
+
     if let Some(land_image_path) = config.land_image_path {
         h3m.set_image(
             land_image_path,
             false,
-            config.one_tile_water,
-            config.obstacles,
-            config.transparent_color,
+            &map_image_params,
         )?;
     }
 
@@ -84,9 +88,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         h3m.set_image(
             underground_image_path,
             true,
-            config.one_tile_water,
-            config.obstacles,
-            config.transparent_color,
+            &map_image_params,
         )?;
     }
 
